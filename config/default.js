5f0dc9cedf16fcdde2d34de335521f3f9c5178a4
Rohit Mishra <rohit.mishra@contentstack.com>
2019-11-11 19:16:45 +0530

    made changes in globalfield module

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 module.exports = {
   versioning: false,
-  host: 'https://dev6-new-api.contentstack.io/v3',
+  host: 'https://stag-api.contentstack.io/v3',
   cdn: 'https://cdn.contentstack.io/v3',
   modules: {
     types: [
