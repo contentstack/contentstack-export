0110df5b4051934d3271e74e9a0a063121006afc
raj pandey <raj.pandey@contentstack.com>
2024-06-27 12:26:14 +0530

    removed secrets

--- config/index.js
+++ config/index.js
@@ -8,9 +8,9 @@ module.exports = {
   email: '',   // (optional)
   password: '', // (optional)
   // Stack API KEY
-  source_stack: 'blt5fc1c50e99b24d80',             // mandatory
-  access_token: '***REMOVED***',
+  source_stack: '',             // mandatory
+  access_token: '',
   management_token: '',    
   // Path where the exported data will be stored (relative path)
-  data: '/Users/raj.pandey/Documents/deprecationRepo/cleanRepo/contentstack-export/exportedContent/contents'
+  data: './contents'
 };
